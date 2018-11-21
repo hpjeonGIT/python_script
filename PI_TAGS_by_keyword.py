aa95cba1179f411951233b55f73e3f1b26652fa7
BySon <bjeon11@gmail.com>
2018-11-21 16:51:40 -0500

    Update PI_TAGS_by_keyword.py

--- PI_TAGS_by_keyword.py
+++ PI_TAGS_by_keyword.py
@@ -17,7 +17,7 @@ piServers = PIServers()
 t1 = time.time()
 ### Modify below VVVVVVVVVVVVVV
 MYSERVERNAME = 'someserver'
-Keyword    = '*engine*'
+Keyword    = '*engine*'            # Be careful not to use '*', which will pull all of PI TAGs
 Time0     = "2018-nov-01 03:45:00" # To use last 2days: "*-2 Day"  To use last 4 hours"*-4h"
 Time1     = "2018-nov-08 13:01:15" # Up to now: "*"
 TimeInterp = "5s"
@@ -27,8 +27,29 @@ PrintTime_1stTagOnly_Inter = True  # Decide to print time stamp in the first col
 ### Modify above ^^^^^^^^^^^^^^
 
 def print_csv(TAGSUCCESS, list_all, type_data):
- 
-
+    if type_data == 'int' and PrintTime_1stTagOnly_Inter:
+        first_only = True
+    else:
+        first_only = False
+    result_all = []
+    columns = []
+    for result, pitag, i in zip(list_all, TAGSUCCESS, range(len(list_all))):
+        a_list = []; b_list = []
+        for event in result:
+            a_list.append(event.Timestamp.LocalTime)
+            b_list.append(str(event.Value))
+        if (first_only and i >0):
+            print("Time stamp is skipped at %s"%(pitag))
+        else:
+            result_all.append(a_list)
+            columns.append('time_'+pitag)
+        result_all.append(b_list)
+        columns.append(pitag)
+    #
+    df = pd.DataFrame(result_all)
+    df = df.transpose()
+    df.columns = columns
+    return df
 
 ## Find and connect PI server
 myserver = None; t0 = time.time()
@@ -41,65 +62,77 @@ if myserver == None:
     sys.exit()
 else:
     print("server %s is found. took %.2f sec\n"%(MYSERVERNAME, time.time() - t0))
-    
+
+# Find PI Tags  
+All_PIpts = PIPoint.FindPIPoints(myserver,Keyword)
+N_All_PIpts = len(list(ALL_PIpts))
+print("Number of found PI TAGs = %d"%(N_All_PIpts))
+MYPITAG = []
+for pitag in all_pipts:
+    MYPITAG.append(pitag)
+  
+#  
 ## Download PI data as recorded - Different tag results are concatenated in the bottom of CSV
 if (Download_Recorded_Data):
-    try:
-        t0 = time.time()
-        list_all = []
-        timerange = AFTimeRange(Time0, Time1)
-        for pitag in MYPITAG:
+    t0 = time.time()
+    list_all = []; TAGSUCCEESS = []
+    timerange = AFTimeRange(Time0, Time1)
+    for pitag, i in zip(MYPITAG, range(len(MYPITAG))):
+        try:
             pt= PIPoint.FindPIPoint(myserver,pitag)
             name = pt.Name
             recorded = pt.RecordedValues(timerange,AFBoundaryType.Inside,"",False)
-            list_all.append(recorded)
-        print("Downloaded recorded data. Took %.2f sec"%(time.time()-t0))
-    except:
-        print('Something is wrong. An error occured.')
-    #    
+            cv = str(pt.CurrentValue()).lower()
+            if (cv not in ['scan off', 'shutdown', 'no data', 'pt created']):
+                list_all.append(recorded)
+                TAGSUCCESS.append(pitag)
+                print('PI TAG %s has been pulled. %d out of %d'%(pitag, i+1, len(MYPITAG)))
+            else:
+                print('PI TAG %s is not active. %d out of %d'%(pitag, i+1, len(MYPITAG)))
+        except:                           
+            print('PI TAG %s is not active. %d out of %d'%(pitag, i+1, len(MYPITAG)))
+    #
     if (len(list_all) > 0):
-        with open('PI_tag_data_recorded.csv','w') as f:
-            for recorded, pitag in zip(list_all,MYPITAG):
-                f.write("# %s\n"%(pitag))
-                for event in recorded:
-                    f.write("%s, %s\n"%(event.Timestamp.LocalTime, str(event.Value)))
-
+        print("Downloaded recorded data. Took %.2f sec"%(time.time() - t0))
+        t0 = time.time()
+        df = print_csv(TAGSUCCESS, list_all, 'rec')
+        tt = time.localtime()
+        fname = 'PI_Data_recorded_' + str(tt.tm_mon) + str(tt.tm_mday) + str(tt.tm_hour) + str(tt.tm_min) + '.csv'
+        df.to_csv(fname)
+        print("Printed recorded data at %s . Took %.2f sec"%(fname, time.time() - t0))
  
 ## Download PI data as interpolation
 if (Download_Interpolated_Data):
     span = AFTimeSpan.Parse(TimeInterp)
     timerange = AFTimeRange(Time0, Time1)            
-    try:
-        t0 = time.time()
-        list_all = []
-        for pitag in MYPITAG:
+    t0 = time.time()
+    list_all = []
+    TAGSUCCESS = []
+    for pitag,i in zip(MYPITAG,range(len(MYPITAG))):
+        try:
             pt= PIPoint.FindPIPoint(myserver,pitag)
             name = pt.Name
             interpolated = pt.InterpolatedValues(timerange, span, "", False)  
-            list_all.append(interpolated)
-        print("Downloaded interpolated data. Took %.2f sec"%(time.time()-t0))
-    except:
-        print("Something is wrong. Interpolation data cannot be downloaded.")
+            cv = str(pt.CurrentValue()).lower()
+            if (cv not in ['scan off', 'shutdown', 'no data', 'pt created']):
+                list_all.append(interpolated)
+                TAGSUCCESS.append(pitag)
+                print('PI TAG %s has been pulled. %d out of %d'%(pitag, i+1, len(MYPITAG)))
+            else:
+                print('PI TAG %s is not active. %d out of %d'%(pitag, i+1, len(MYPITAG)))
+        except:                           
+            print('PI TAG %s is not active. %d out of %d'%(pitag, i+1, len(MYPITAG)))
     #
     # Now we make one big table or dataframe
     if (len(list_all) > 0):
-        columns = []
-        for pitag in MYPITAG:
-            columns.append('time_'+pitag)
-            columns.append(pitag)
-        #
-        result_all = []
-        for interpolated in list_all:
-            a_list = [];b_list = []
-            for event in interpolated:
-                a_list.append(event.Timestamp.LocalTime)
-                b_list.append(str(event.Value))
-            result_all.append(a_list); result_all.append(b_list)
-        #
-        df= pd.DataFrame(result_all)
-        df = df.transpose()
-        df.columns = columns
-        df.to_csv('PI_TAG_data_interpol.csv')
+        print("Downloaded recorded data. Took %.2f sec"%(time.time() - t0))
+        t0 = time.time()
+        df = print_csv(TAGSUCCESS, list_all, 'int')
+        tt = time.localtime()
+        fname = 'PI_Data_interp_' + str(tt.tm_mon) + str(tt.tm_mday) + str(tt.tm_hour) + str(tt.tm_min) + '.csv'
+        df.to_csv(fname)
+        print("Printed interpolated data at %s . Took %.2f sec"%(fname, time.time() - t0))
+        print("Number of valid PI TAGs = %d. The size of the produced table is %d x %d"%(len(TAGSUCCESS), df.shape[0], df.shape[1]))
         
 print("Elapsed time = %.2f sec"%(time.time()-t1))
 

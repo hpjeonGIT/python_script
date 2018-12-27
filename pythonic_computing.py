b28ef15358b79f02672a1743d7eb93bc191d152e
Byson <bjeon11@gmail.com>
2018-12-27 15:04:09 -0500

    Update pythonic_computing.py

--- pythonic_computing.py
+++ pythonic_computing.py
@@ -30,34 +30,84 @@ print(len(bf), bf[0:10],time.time() - t0)
 # This takes 0.039 sec
 ''' Fortran version
 program test
+  use omp_lib
   implicit none
-  integer::N, i, j
+  integer::N, i, j, ierr, m
   real:: t1, t2
   real*8:: tmp
   integer,allocatable:: id(:), find(:)
   real*8,allocatable:: x(:), y(:)
-  N = 2000000
-  allocate(id(N), x(N), y(N), find(N))
+  type aos
+     integer:: id
+     real*8:: x,y
+  end type aos
+  type soa
+     integer,allocatable:: id(:)
+     real*8, allocatable:: x(:), y(:)
+  end type soa
+  type(aos), allocatable:: q(:)
+  type(soa):: p
+  
+  N = 1000000
+  allocate(id(N), x(N), y(N), find(N), stat=ierr)
+  allocate(q(N), p%id(N), p%x(N), p%y(N),stat=ierr)
   open(unit=10,file='sample.dat')
   read(10,*)
   do i = 1, N
      read(10,*), j, tmp, x(i), y(i)
      id(i) = int(tmp)
+     q(i)%id = id(i)
+     q(i)%x  = x(i)
+     q(i)%y  = y(i)
+     p%id(i) = id(i)
+     p%x( i) = x(i)
+     p%y( i) = y(i)
   end do
   close(10)
   find(:) = 0
   !
-  j = 0
   t1 = secnds (0.0)
-  do i =1, N
-     if (x(i) > 0.5 .or. y(i) > 0.5) then
-        j = j + 1
-        find(j) = id(i)
-     end if
+  do m=1,100
+     find(:) = 0
+     j = 0
+     do i =1, N
+        if (x(i) > 0.5 .or. y(i) > 0.5) then
+           j = j + 1
+           find(j) = id(i)
+        end if
+     end do
   end do
   !
   t2 = secnds (t1)
-  print*, j, find(1:10), t2 ! took 0.0156 sec
-  deallocate(id,x,y, find)
+  print*, 'separate arrays', j,  t2 ! took 0.66sec
+  !
+  t1 = secnds (0.0)
+  do m=1,100
+     j = 0; find(:) = 0
+     do i =1, N
+        if (q(i)%x > 0.5 .or. q(i)%y > 0.5) then
+           j = j + 1
+           find(j) = q(i)%id
+        end if
+     end do
+  end do
+  !
+  t2 = secnds (t1)
+  print*, 'AoS', j, t2  ! 0.70 sec
+  ! 
+  t1 = secnds (0.0)
+  do m=1,100
+     j = 0; find(:) = 0
+     do i =1, N
+        if (p%x(i) > 0.5 .or. p%y(i) > 0.5) then
+           j = j + 1
+           find(j) = p%id(i)
+        end if
+     end do
+  end do
+  !
+  t2 =  secnds (t1)
+  print*, 'SoA', j,  t2 ! 0.66 sec
+  deallocate(id,x,y, find, q, p%id, p%x, p%y)
 end program test
 '''
